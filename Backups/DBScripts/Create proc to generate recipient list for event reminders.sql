5f51007691bb49c959097ecf9145be764022046b
Joseph Bolla <joseph.bolla@immediate.co.uk>
2014-10-23 16:52:41 +0100

    more work on scheduled tasks

---
+++ Backups/DBScripts/Create proc to generate recipient list for event reminders.sql
@@ -0,0 +1,107 @@
+
+create proc BuildCalEventReminderRecipientList
+
+ @eventid int,  -- feed as parameter from proc
+ @companyid int, -- feed as param
+ @ListName nvarchar(250), 
+ @description nvarchar(500),
+ @CreatedBy int
+ 
+As
+Begin
+
+-- declaration
+declare @eventtargexml as xml
+declare @targetypeid int
+declare @val nvarchar(max)
+declare @creatorId int
+declare @currentemail nvarchar(150)
+declare @currentmobile nvarchar(20)
+declare @recipientListId int
+
+
+-- create recipient list first
+insert into RecipientList
+select @ListName, @description, GETDATE(), @createdby, null, null, @eventid
+
+select @recipientListId = @@IDENTITY
+
+select @creatorId = u.id, @currentemail = u.email, @currentmobile = ISNULL(u.Mobile, u.Phone) 
+from CalendarEvent ca 
+inner join [User]  u on ca.CreatedBy = u.id
+where ca.id = @eventid 
+
+select @eventtargexml = cast(AssociationValue as XML)
+from CalendarAssociation
+where EventId = @eventid
+
+select @targetypeid = feed.x.value('.','int')
+from @eventtargexml.nodes('//target/type') feed(x)
+
+select @val = feed.x.value('.','nvarchar(max)')
+from @eventtargexml.nodes('//target/value') feed(x)
+
+select @targetypeid
+
+if not @targetypeid is null
+begin
+	if @targetypeid = 251 -- company
+	begin
+		insert into Recipient
+		select u.id, u.email, ISNULL(u.Mobile, u.Phone),  0, null, @recipientListId
+		from [User]  u
+		where u.CompanyId = @companyid and u.active = 1
+	end
+	else if @targetypeid = 252 -- department
+	begin
+		--*** loop through department list
+		-- *** insert users for each department listed. (active users only)
+		declare @tempdept as table(departmentid int)
+		insert into @tempdept
+		select * from [dbo].[SplitCSV](@val, ',')
+		
+		insert into Recipient
+		select distinct u.id, u.email, ISNULL(u.Mobile, u.Phone),  0, null, @recipientListId
+		from [User]  u
+		inner join @tempdept tt on tt.departmentid = u.departmentid
+		where u.active = 1 
+		
+		if not exist (select 1 from Recipient where RecipientUserId = @CreatedBy and RecipientListId = @recipientListId)
+		begin
+			insert into Recipient
+			select u.id, u.email, ISNULL(u.Mobile, u.Phone),  0, null, @recipientListId
+			from [User] where id = @CreatedBy and active = 1
+		end
+		
+	end
+	else if @targetypeid = 253 -- User
+	begin
+		declare @temp as table(userid int)
+
+		insert into @temp
+		select * from [dbo].[SplitCSV](@val, ',')
+		-- add creator to list
+		insert into @temp
+		select @CreatedBy
+		
+		insert into Recipient
+		select distinct u.id, u.email, ISNULL(u.Mobile, u.Phone),  0, null, @recipientListId
+		from [User]  u
+		inner join @temp tt on tt.userid = u.id
+		where u.active = 1 
+		
+	end
+	else if @targetypeid = 254 -- MyselfOnly
+	begin
+		
+		insert into Recipient
+		select u.id, u.email, ISNULL(u.Mobile, u.Phone),  0, null, @recipientListId
+		from [User]  u 
+		where u.id = @CreatedBy and u.active = 1		
+		
+	end
+	
+	
+end
+
+End
--- Backups/DBScripts/Script to generate scheduled notifications.sql
+++ Backups/DBScripts/Script to generate scheduled notifications.sql
@@ -1,20 +1,10 @@
 
-
-
---GET the frequencytype of reminder
-
--- based on frequency, get duration
-
--- get event details
-
--- compute time to duration of reminder and start of event
-
--- drop entry into scheduled task table (check notification type)
-
--- service will then read from that table to send notification
-
--- create email template for reminders
-
+Create Proc PrepareCalendarEventScheduledReminder
+ @SenderName nvarchar(100), -- send from current app
+ @SenderEmail nvarchar(150), -- send from current app
+ @RecipientListId int -- send from current app
+as
+begin
 
 declare @EventId int
 declare @associationid int
@@ -24,10 +14,12 @@ declare @Description nvarchar(4000)
 declare @location nvarchar(50)
 declare @startofevent datetime
 declare @endofevent datetime
+declare @reminderStartDate datetime
 declare @remindertype int
 declare @frequencytypeid int
 declare @frequency int
 declare @notificationtype int
+
 
 declare @ReminderTempTab as Table(AssociationId int, EventId int, CalendarId int, CreatorId int, AssociationValue nvarchar(max),
 	EventTitle nvarchar(100), [Description] nvarchar(500), Location nvarchar(50), StartofEvent datetime, EndofEvent datetime)
@@ -40,41 +32,50 @@ select ca.EventAssociationId, ca.EventId, ce.CalendarId, ce.CreatedBy, ca.Associ
 	ce.Title, ce.[Description], ce.Location, ce.StartOfEvent, ce.EndOfEvent
 from CalendarAssociation ca
 inner join CalendarEvent ce on ce.id = ca.EventId
-where AssociationTypeid = 2 -- reminders only
+left join ScheduledNotifications sn on sn.ObjectId = ca.EventAssociationId
+where AssociationTypeid = 2 and sn.ObjectId is null -- reminders only
 
--- take next row from the pool
-select top 1  @associationid = AssociationId ,@EventId = EventId,@AssociationValue = AssociationValue, @Eventtitle = EventTitle, @Description = Description,
-@location = Location, @startofevent = StartofEvent, @endofevent = EndofEvent
-from @ReminderTempTab
+	-- take next row from the pool
+	while ((select COUNT(associationid) from @ReminderTempTab) > 0)
+	Begin
 
--- remove processed row
-delete from @ReminderTempTab where AssociationId = @associationid
+		select top 1  @associationid = AssociationId ,@EventId = EventId,@AssociationValue = AssociationValue, @Eventtitle = EventTitle, @Description = [Description],
+		@location = Location, @startofevent = StartofEvent, @endofevent = EndofEvent
+		from @ReminderTempTab
 
+		-- remove processed row
+		delete from @ReminderTempTab where AssociationId = @associationid
 
-insert into @tempxmltable select CAST(@AssociationValue as XML)
+		insert into @tempxmltable select CAST(@AssociationValue as XML)
 
+		SELECT 
+			@remindertype = val.value('(/reminder/type)[1]', 'int'), 
+			@frequencytypeid = val.value('(/reminder/frequencytype)[1]', 'int'), 
+			@frequency = val.value('(/reminder/frequency)[1]', 'int'),
+			@notificationtype = val.value('(/reminder/notification)[1]', 'int')
+		FROM
+			@tempxmltable		    
+		   
+		   select    
+		   @frequencytypeid = (case when @remindertype <> 16 then frequencytype else @frequencytypeid end) , 
+		   @frequency = (case when @remindertype <> 16 then frequency else @frequency end) 
+		   from CalEventReminderType
+		   where id = @remindertype
 
-SELECT 
-    @remindertype = val.value('(/reminder/type)[1]', 'int'), 
-    @frequencytypeid = val.value('(/reminder/frequencytype)[1]', 'int'), 
-    @frequency = val.value('(/reminder/frequency)[1]', 'int'),
-    @notificationtype = val.value('(/reminder/notification)[1]', 'int')
-FROM
-    @tempxmltable
-    
-   
-   select @remindertype, @frequencytypeid, @frequency, @notificationtype
-   
-   return;
-   
---select 257,
+		
+		select @reminderStartDate =
+		 case when @frequencytypeid = 1 then DateAdd(minute, -1 * @frequency, @startofevent) 
+		 when @frequencytypeid = 2 then DateAdd(HOUR, -1 * @frequency, @startofevent)
+		 when @frequencytypeid = 3 then DateAdd(DAY, -1 * @frequency, @startofevent)  
+		 when @frequencytypeid = 4 then DateAdd(WEEK, -1 * @frequency, @startofevent)
+		 when @frequencytypeid = 5 then DateAdd(MONTH, -1 * @frequency, @startofevent)
+		 end
 
+		-- insert schedules task
+		insert into scheduledNotifications
+		select 257, @notificationtype, @associationid, @Description, @Eventtitle, @RecipientListId, @reminderStartDate,1,GETDATE(),
+			@SenderName, @SenderEmail
 
---insert into scheduledNotifications
- -- <reminder><type>3</type><frequencytype>0</frequencytype><frequency>0</frequency><notification>1</notification></reminder>
+	End
 
---select * from Glossary
---select * from scheduledNotifications  
---select * from CalendarAssociation
-
---select * from Glossary
+End
