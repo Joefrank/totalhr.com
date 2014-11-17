614b8eb7a5ffe773afe3e3d71bebc1529ca05920
Joseph Bolla <admin@cyberminds.co.uk>
2014-11-17 00:02:44 +0000

    Added file manament service updates and new views

---
+++ FileManagementService/Infrastructure/IFileService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using totalhr.data;
+using totalhr.data.EF;
+
+namespace FileManagementService.Infrastructure
+{
+    public interface IFileService
+    {
+        int Create(File file);
+
+        int Create(HttpPostedFileBase IOfile, string destinationFolder, int createdBy, int fileTypeId);
+
+        int UpdateWith(int FileId, HttpPostedFileBase IOfile, string destinationFolder, int createdBy, int fileTypeId);
+
+        byte[] ReadFileBytes(string filePath);
+    }
+}
---
+++ totalhr.services/Infrastructure/ICompanyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using totalhr.data.EF;
+using totalhr.data.Models;
+using totalhr.Shared;
+using totalhr.Shared.Models;
+
+namespace totalhr.services.Infrastructure
+{
+    public interface ICompanyService
+    {
+
+        List<Department> GetCompanyDepartments(int companyid);
+
+        List<string> GetCompanyDepartmentsByIds(List<int> ids);
+      
+    }
+}
