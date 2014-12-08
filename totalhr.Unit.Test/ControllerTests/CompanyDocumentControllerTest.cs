0edc7667a02c7968962e364014bcb6f468540447
Joseph Bolla <joseph.bolla@immediate.co.uk>
2014-12-08 14:29:28 +0000

    Restructuring for unit tests

---
+++ totalhr.Unit.Test/ControllerTests/CompanyDocumentControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using totalhr.web.Controllers;
+
+namespace totalhr.Unit.Test.ControllerTests
+{
+    [TestClass]
+    public class CompanyDocumentControllerTest
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            var controller = new DocumentController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+    }
+}
