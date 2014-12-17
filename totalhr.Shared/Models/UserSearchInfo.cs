92c204780a51444316b29ac160dd31ff11c08bfe
Joseph Bolla <admin@cyberminds.co.uk>
2014-12-17 21:44:12 +0000

    Add missing files

---
+++ totalhr.Shared/Models/UserSearchInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totalhr.Shared.Models
+{
+    public class UserSearchInfo
+    {
+        public int Id { get; set; }
+        public int UserTypeId { get; set; }
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PartialAddress { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+        public string Phone { get; set; }
+        public int LanguageId { get; set; }
+
+        public IEnumerable<ListItemStruct> UserList { get; set; }
+        public IEnumerable<ListItemStruct> DepartmentList { get; set; }
+    }
+}
---
+++ totalhr.web/areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Authentication.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using totalhr.data.EF;
+using totalhr.services.Infrastructure;
+using totalhr.Shared.Models;
+using totalhr.web.Areas.Admin.Models;
+
+
+namespace totalhr.web.Areas.Admin.Controllers
+{
+    public class UserController : AdminBaseController
+    {
+        private readonly IAccountService _accountService;
+        private readonly IProfileService _profileService;
+        private readonly ICompanyService _companyService;
+
+        public UserController(IAccountService accountService, IProfileService profileService, ICompanyService companyService, IOAuthService authService) :
+            base(authService)
+        {
+            _accountService = accountService;
+            _profileService = profileService;
+            _companyService = companyService;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.SearchInfo = new UserSearchInfo
+            {
+                UserList = _accountService.ListCompanyUsersSimple(CurrentUser.CompanyId),
+                DepartmentList = _companyService.GetDepartmentSimple(CurrentUser.CompanyId)
+            };
+
+            var test = _accountService.GetUserListForAdmin(null, CurrentUser.LanguageId);
+            return View(test);
+        }
+
+        public ActionResult UserDetails(string uniqueid)
+        {
+            return View(_accountService.GetUserDetailsForAdmin(uniqueid));
+        }
+
+        public ActionResult SearchForUser(UserSearchInfo info)
+        {
+            var result = new UserSearchResult
+            {
+                FoundUsers = _accountService.SearchUsers(info),
+                SearchInfo = info
+            };
+            return View("SearchResult", result);
+        }
+    }
+}
