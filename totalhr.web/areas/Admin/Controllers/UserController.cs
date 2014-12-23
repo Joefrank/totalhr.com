e270b062770d658b380d89e43cbbb6a69a01d848
Joseph Bolla <joseph.bolla@immediate.co.uk>
2014-12-23 12:33:25 +0000

    fixed user save

--- totalhr.web/areas/Admin/Controllers/UserController.cs
+++ totalhr.web/areas/Admin/Controllers/UserController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 using totalhr.data.EF;
 using totalhr.services.Infrastructure;
 using totalhr.Shared;
 using totalhr.Shared.Models;
 using totalhr.web.Areas.Admin.Models;
+using totalhr.Resources;
 
 
 namespace totalhr.web.Areas.Admin.Controllers
@@ -19,6 +21,8 @@ namespace totalhr.web.Areas.Admin.Controllers
         private readonly IProfileService _profileService;
         private readonly ICompanyService _companyService;
         private readonly IGlossaryService _glossaryService;
+
+        private static readonly ILog Log = LogManager.GetLogger(typeof(AccountController));
 
         public UserController(IGlossaryService glossaryService, IAccountService accountService, IProfileService profileService, ICompanyService companyService, IOAuthService authService) :
             base(authService)
@@ -87,6 +91,7 @@ namespace totalhr.web.Areas.Admin.Controllers
             ViewBag.CountryList = _glossaryService.GetGlossary(this.ViewingLanguageId, Variables.GlossaryGroups.Country);
             ViewBag.GenderList = _glossaryService.GetGlossary(this.ViewingLanguageId, Variables.GlossaryGroups.Gender);
             ViewBag.TitleList = _glossaryService.GetGlossary(this.ViewingLanguageId, Variables.GlossaryGroups.Title);
+            ViewBag.DepartmentList = _companyService.GetDepartmentSimple(CurrentUser.CompanyId);
         }
 
         public ActionResult Create()
@@ -98,29 +103,30 @@ namespace totalhr.web.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult Create(NewEmployeeInfo info)
         {
+            if (info.DepartmentId < 1)
+            {
+                ModelState.AddModelError("Missing_Department", Error.Error_Missing_Department); 
+            }
+
             if (ModelState.IsValid)
             {
-                //register user.
-                User user = _accountService.CreateUser(info);
+                //create user. *** set some admin tasks for profiles, roles and picture.
+                //*** review profile, permissions and roles
+                var user = _accountService.CreateUser(info);
 
-                if (structresult.UserId > 0 && structresult.CompanyId > 0)
+                if (user != null && user.id > 0)
                 {
-                    //Log.Debug("Notification/Messaging started");
-
-                    //send notifications to both admin and registrant.
-                    _messagingService.AcknowledgeNewUserRegistration(WebsiteKernel, structresult);
-                    //prepare and redirect to welcome screen.
-                    TempData["UserReg"] = structresult;
-                    return RedirectToAction("Welcome", "Account");
+                    return View("UserCreated", info);
                 }
                 else
                 {
-                    ModelState.AddModelError("RegistrationFailed", structresult.RegError);
-                    Log.Debug("New User Registration failed " + structresult.RegError);
+                    ModelState.AddModelError("RegistrationFailed", Error.Error_Could_Not_CreateUser);
+                    Log.Debug("Employee Registration failed ");
                 }
             }
 
-            return RedirectToAction("Index");
+            LoadGlossaries();
+            return View();
         }
     }
 }
