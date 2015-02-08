d4eeaae62e4bc2158b9a6b8c80aca7e0f550d5cc
Joseph Bolla <admin@cyberminds.co.uk>
2015-02-08 10:04:01 +0000

    changes for new layout

---
+++ totalhr.web/areas/Admin/Models/BreadCrumbItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace totalhr.web.Areas.Admin.Models
+{
+    public class BreadCrumbItem
+    {
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+}
