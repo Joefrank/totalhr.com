9b33b1bbea53874b45b1fdce0c28e44d90f849f9
Joseph Bolla <joseph.bolla@immediate.co.uk>
2015-02-26 23:07:22 +0000

    side menu update

---
+++ totalhr.web/Controllers/AbsenceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Authentication.Infrastructure;
+using ChatService.Infrastructure;
+
+namespace totalhr.web.Controllers
+{
+    public class AbsenceController : BaseController
+    {
+        private IOAuthService _authService;
+
+        public AbsenceController(IOAuthService authservice)
+            : base(authservice)
+        {
+            _authService = authservice;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
