456d91b1e19a34b8e8604939d38a5df1d813870f
Joseph Bolla <admin@cyberminds.co.uk>
2015-02-27 13:12:49 +0000

    tidying up layouts

---
+++ totalhr.web/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace totalhr.web.Controllers
+{
+    public class TaskController : Controller
+    {
+        //
+        // GET: /Task/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- totalhr.web/Scripts/Site/Chat.js
+++ totalhr.web/Scripts/Site/Chat.js
@@ -1,111 +1,85 @@
-﻿$(document).ready(function () {
-
-    //$("#txtNickName").val("").focus();
-
-    //$("#btnLogin").click(function () {
-    //    var nickName = $("#txtNickName").val();
-    //    if (nickName) {
-    //        //call the Index method of the controller and pass the attribute "logOn"
-    //        var href = "/Chat?user=" + encodeURIComponent(nickName);
-    //        href = href + "&logOn=true";
-    //        $("#LoginButton").attr("href", href).click();
-
-    //        //the nickname is persisted here
-    //        $("#YourNickname").text(nickName);
-    //    }
-    //});
-
-    ////auto click when enter is pressed
-    //$('#txtNickName').keydown(function(e) {
-    //    if (e.keyCode == 13) {
-    //        e.preventDefault();
-    //        $("#btnLogin").click();
-    //    }
-    //});
-
-    Refresh();
-});
+﻿
 
 //the login was successful. Setup events for the lobby and prepare other UI items
-function LoginOnSuccess(result) {
-    alert('success');
-    ScrollChat();
-    ShowLastRefresh();
+//function LoginOnSuccess(result) {
+//    alert('success');
+//    ScrollChat();
+//    ShowLastRefresh();
 
-    $("#txtSpeak").val('').focus();
+//    $("#txtSpeak").val('').focus();
 
-    //the chat state is fetched from the server every 5 seconds (ping)
-    setTimeout("Refresh();", 5000);
+//    //the chat state is fetched from the server every 5 seconds (ping)
+//    setTimeout("Refresh();", 5000);
 
-    //auto post when enter is pressed
-    $('#txtSpeak').keydown(function (e) {
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            $("#btnSpeak").click();
-        }
-    });
+//    //auto post when enter is pressed
+//    $('#txtSpeak').keydown(function (e) {
+//        if (e.keyCode == 13) {
+//            e.preventDefault();
+//            $("#btnSpeak").click();
+//        }
+//    });
 
-    //setup the event for the "Speak" button that is rendered in the partial view 
-    $("#btnSpeak").click(function () {
-        var text = $("#txtSpeak").val();
-        if (text) {
+//    //setup the event for the "Speak" button that is rendered in the partial view 
+//    $("#btnSpeak").click(function () {
+//        var text = $("#txtSpeak").val();
+//        if (text) {
 
-            //call the Index method of the controller and pass the attribute "chatMessage"
-            var href = "/Chat?user=" + encodeURIComponent($("#YourNickname").text());
-            href = href + "&chatMessage=" + encodeURIComponent(text);
-            $("#ActionLink").attr("href", href).click();
+//            //call the Index method of the controller and pass the attribute "chatMessage"
+//            var href = "/Chat?user=" + encodeURIComponent($("#YourNickname").text());
+//            href = href + "&chatMessage=" + encodeURIComponent(text);
+//            $("#ActionLink").attr("href", href).click();
 
-            $("#txtSpeak").val('').focus();
-        }
-    });
+//            $("#txtSpeak").val('').focus();
+//        }
+//    });
 
 
-    //setup the event for the "Speak" button that is rendered in the partial view 
-    $("#btnLogOff").click(function () {
+//    //setup the event for the "Speak" button that is rendered in the partial view 
+//    $("#btnLogOff").click(function () {
 
-        //call the Index method of the controller and pass the attribute "logOff"
-        var href = "/Chat?user=" + encodeURIComponent($("#YourNickname").text());
-        href = href + "&logOff=true";
-        $("#ActionLink").attr("href", href).click();
+//        //call the Index method of the controller and pass the attribute "logOff"
+//        var href = "/Chat?user=" + encodeURIComponent($("#YourNickname").text());
+//        href = href + "&logOff=true";
+//        $("#ActionLink").attr("href", href).click();
 
-        document.location.href = "Chat";
-    });
+//        document.location.href = "Chat";
+//    });
 
-}
+//}
 
 //briefly show login error message
-function LoginOnFailure(result) {
-    alert('failure');
-    $("#YourNickname").val("");
-    $("#Error").text(result.responseText);
-    //setTimeout("$('#Error').empty();", 2000);
-}
+//function LoginOnFailure(result) {
+//    alert('failure');
+//    $("#YourNickname").val("");
+//    $("#Error").text(result.responseText);
+//    //setTimeout("$('#Error').empty();", 2000);
+//}
 
 //called every 5 seconds
-function Refresh() {
+//function Refresh() {
     
-    $('#result').load('ajax/test.html', function () {
-        alert('Load was performed.');
-    });
+//    $('#result').load('ajax/test.html', function () {
+//        alert('Load was performed.');
+//    });
     
-    var href = "/Chat/RefreshMessages/3"; // + encodeURIComponent($("#YourNickname").text());
+//    var href = "/Chat/RefreshMessages/3"; // + encodeURIComponent($("#YourNickname").text());
 
-    //call the Index method of the controller
-    $("#ActionLink").attr("href", href).click();
-    setTimeout("Refresh();", 5000);
-}
+//    //call the Index method of the controller
+//    $("#ActionLink").attr("href", href).click();
+//    setTimeout("Refresh();", 5000);
+//}
 
 //Briefly show the error returned by the server
-function ChatOnFailure(result) {
-    $("#Error").text(result.responseText);
-    setTimeout("$('#Error').empty();", 2000);
-}
+//function ChatOnFailure(result) {
+//    $("#Error").text(result.responseText);
+//    setTimeout("$('#Error').empty();", 2000);
+//}
 
-//Executed when a successful communication with the server is finished
-function ChatOnSuccess(result) {
-    ScrollChat();
-    ShowLastRefresh();
-}
+////Executed when a successful communication with the server is finished
+//function ChatOnSuccess(result) {
+//    ScrollChat();
+//    ShowLastRefresh();
+//}
 
 //scroll the chat window to the bottom
 function ScrollChat() {
@@ -121,3 +95,74 @@ function ShowLastRefresh() {
     $("#LastRefresh").text("Last Refresh - " + time);
 }
 
+/* my functions */
+function GetCurrentTime() {
+    var time = new Date();
+    return (time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+}
+
+function Refresh() {
+    $('#RefreshArea').load('/Chat/RefreshMessages/' + chatRoomId, function () {
+        //alert('Load was performed.');
+    });
+
+    LogoutCountDown--;
+    if (LogoutCountDown < 1) {
+        Loggout();
+        return;
+    }
+    setTimeout("Refresh();", 5000);
+    $('#spLastRefresh').html(MSG_LastRefreshAt.replace('{0}', GetCurrentTime()));
+}
+
+function SendMessage() {
+    var message = encodeURIComponent($('#txtMessage').val());
+
+    if (message === '')
+        return;
+
+    var formData = { RoomId: chatRoomId, UserId: 0, Nickname: "", Message: message }; //Array
+
+    $.ajax({
+        url: "/Chat/PostMessage/",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            $('#txtMessage').val('');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
+        }
+    });
+
+    //reset logout count
+    LogoutCountDown = LOGOUT_MAX;
+}
+
+function Loggout() {
+    var formData = { RoomId: chatRoomId, UserId: 0, Nickname: "", Message: "" }; //Array
+
+    $.ajax({
+        url: "/Chat/LogoutUser/",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+            //alert(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            //alert(textStatus);
+        }
+    });
+}
+
+$(document).ready(function () {
+    Refresh();
+
+    $('#txtMessage').keydown(function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            SendMessage();
+        }
+    });
+
+});
